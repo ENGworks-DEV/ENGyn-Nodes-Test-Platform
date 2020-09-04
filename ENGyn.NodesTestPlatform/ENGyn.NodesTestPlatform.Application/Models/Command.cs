4d2fba6bf13249c92f80bc9cfa21bc38c274fe17
moisespinate <62384430+moisespinate@users.noreply.github.com>
2020-09-04 14:42:41 -0400

    EN-676 / Added Application Layer

---
+++ ENGyn.NodesTestPlatform/ENGyn.NodesTestPlatform.Application/Models/Command.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENGyn.NodesTestPlatform.Application.Models
+{
+    public class Command
+    {
+        public string Name { get; set; }
+        public string LibraryClassName { get; set; }
+        public IEnumerable<string> Arguments { get; set; }
+    }
+}
