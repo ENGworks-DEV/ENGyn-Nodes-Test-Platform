48f83e44ed7424b21b0198be26e5eead104b57da
moisespinate <62384430+moisespinate@users.noreply.github.com>
2020-09-08 11:37:10 -0400

    Added Default commands class

---
+++ ENGyn.NodesTestPlatform/ENGyn.NodesTestPlatform/Commands/DefaultCommands.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENGyn.NodesTestPlatform.Commands
+{
+    public static class DefaultCommands
+    {
+        public static string Test(int id, string data)
+        {
+            return string.Format("Test executed to method {0} with data {1}", id, data);
+        }
+
+        public static string GetDate(DateTime date)
+        {
+            return string.Format("Date and time is: {0}", date);
+        }
+    }
+}
