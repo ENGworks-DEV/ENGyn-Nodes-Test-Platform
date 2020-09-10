db229156be336e02b4cba2e7e131b37ff352985c
moisespinate <62384430+moisespinate@users.noreply.github.com>
2020-09-10 01:18:17 -0400

    Adding some code to"Execute" method. (On progress)

--- ENGyn.NodesTestPlatform/ENGyn.NodesTestPlatform/Providers/RunnableProvider.cs
+++ ENGyn.NodesTestPlatform/ENGyn.NodesTestPlatform/Providers/RunnableProvider.cs
@@ -14,16 +14,21 @@ namespace ENGyn.NodesTestPlatform.Providers
     public class RunnableProvider : IRunnableService
     {
         private string _art;
-        private Dictionary<string, Dictionary<string, IList<ParameterInfo>>> _commandLibaries;
         private CommandsLoader _commandsLoader;
+        private CommandValidation _commandValidation;
+        private Dictionary<string, Dictionary<string, IList<ParameterInfo>>> _commandLibaries;
 
         public RunnableProvider()
         {
+            _commandValidation = new CommandValidation();
             _commandsLoader = new CommandsLoader();
             _commandLibaries = _commandsLoader.LoadAndGetLibraries();
             _art = DesignArt.CreateArt();
         }
 
+        /// <summary>
+        /// Main execution loop of the application
+        /// </summary>
         public void Run()
         {
             ConsolePrompt.WriteToConsole(_art, ConsoleColor.Green);
@@ -46,7 +51,59 @@ namespace ENGyn.NodesTestPlatform.Providers
 
         public string Execute(Command command)
         {
-            return string.Format("Executed the {0} command", command.Name);
+            // Validate command
+            bool validCommandLibraryFlag = _commandValidation.ValidateLibraryCommand(_commandLibaries, command.LibraryClassName, command.Name);
+
+            if (validCommandLibraryFlag)
+            {
+                // command arguments list
+                var argumentValueList = new List<object>();
+                var argumentList = _commandLibaries[command.LibraryClassName][command.Name];
+
+                // Validate argument count
+                var validArgsCountFlag = _commandValidation.ValidateProvidedArgumentsCount(argumentList, command.Arguments.Count());
+                
+                // Check for invalid argument count
+                if(!validArgsCountFlag)
+                {
+                    throw new ArgumentException("Missing required argument. Use command 'help' to see commands info");
+                }
+
+                // If command contains arguments
+                if (argumentList.Count > 0)
+                {
+                    foreach (var argument in argumentList)
+                    {
+                        argumentValueList.Add(argument.DefaultValue);
+                    }
+                    
+                    for(int index = 0; index < command.Arguments.Count(); index++ )
+                    {
+                        var commandArgument = argumentList.ElementAt(index);
+                        var argumentType = commandArgument.ParameterType;
+                        
+                        try
+                        {
+                            // TODO argument reasign and Convertion to right parameter type
+                            // Fun fact: Create a generic Tryparse to convert from string to any primitive type of C#
+                        }
+                        catch (Exception)
+                        {
+                            throw;
+                        }
+                    }
+                }
+            } 
+            else
+            {
+                throw new ArgumentException(@"Unknown command. Use command 'help' to see commands info");
+            }
+
+            // Getting params based on their characteristics
+            var requiredParams = paramInfoList.Where(p => p.IsOptional == false);
+            var optinalParams = paramInfoList.Where(p => p.IsOptional == true);
+
+            return "TODO finalize execute";
         }
     }
 }
