6a9c71d2c38ad6b339e7b20d0abd87a0f3adc9d1
moisespinate <62384430+moisespinate@users.noreply.github.com>
2020-09-10 18:03:33 -0400

    Added classes for command handling and Parse datatypes

---
+++ ENGyn.NodesTestPlatform/ENGyn.NodesTestPlatform/Core/CommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using ENGyn.NodesTestPlatform.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENGyn.NodesTestPlatform.Core
+{
+    public class CommandHandler
+    {
+        private readonly string _commandsNamespace = "ENGyn.NodesTestPlatform.Commands";
+
+        public string InvokeConsoleCommand(Command userCommand, object[] args)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Type commandLibraryClass = assembly.GetType($"{_commandsNamespace}.{userCommand.LibraryClassName}");
+
+            try
+            {
+                var result = commandLibraryClass.InvokeMember(
+                    userCommand.Name,
+                    BindingFlags.Public | BindingFlags.Static | BindingFlags.InvokeMethod,
+                    null, null,
+                    args
+                );
+
+                return result.ToString();
+            }
+            catch (TargetInvocationException ex)
+            {
+                throw ex.InnerException;
+            }
+        }
+    }
+}
